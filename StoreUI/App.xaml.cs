59f931db5859e6195fa82a0e4e233b5ba3c42083
niklas-hjelm <niklas.hjelm@iths.se>
2023-12-04 21:30:24 +0100

    added wpf

---
+++ StoreUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace StoreUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ StoreUI/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using DataAccess;
+
+namespace StoreUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            using var context = new ProductContext();
+            var products = context.Products.ToList();
+            foreach (var product in products)
+            {
+                ProdList.Items.Add(product.Name);
+            }
+        }
+    }
+}
